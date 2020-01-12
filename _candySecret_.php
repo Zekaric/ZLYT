29024c664307ffe29f6f9d5a93a917fc8c4b86e4
Robbert de Groot <zekaric@yahoo.ca>
2020-01-12 13:53:25 -0800

    Cleaning up unused stuff.

--- _candySecret_.php
+++
@@ -1,3 +0,0 @@
-$googleCredetialId = "ZekaricWebSiteLogin";
-$googleId          = "639817765622-b49qqr8gct5cgikcenq56pk9nl6d8f2i.apps.googleusercontent.com";
-$googleSecret      = "vL0Qj_smf4ZWVZYEE5ZSfw9T";
